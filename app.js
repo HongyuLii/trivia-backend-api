3910b5a91892cbcc5d4831e15fc45e86d2063fd3
Hongyu Li <hongyu.li@jitsuin.com>
2021-04-04 09:26:03 -0400

    another

--- app.js
+++ app.js
@@ -13,15 +13,26 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Connection to DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/questiontester', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log('Backend Started');
-    })
-    .catch(err => {
-        console.error('Backend error:', err.stack);
-        process.exit(1);
-    });
+const MONGODB_URI = "mongodb+srv://firstUser:LIHONGyu19980918@cluster0.wg0g5.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+// // Connection to DB
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/questiontester', { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//       console.log('Backend Started');
+//     })
+//     .catch(err => {
+//         console.error('Backend error:', err.stack);
+//         process.exit(1);
+//     });
+
+
+//connect Database.
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Mongodb database connection is on");
+});
 
 const questionsRouter = require('./routes/questions');
 const usersRouter = require('./routes/users');
@@ -32,3 +43,5 @@ app.use('/users', usersRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+module.exports = app
