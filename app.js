839b1301f462db88e1e99ffec33f843fdaddb92d
Hongyu Li <hongyu.li@jitsuin.com>
2021-04-05 09:22:17 -0400

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 8000;
 
 // App Instance
@@ -13,26 +13,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const MONGODB_URI = "mongodb+srv://firstUser:LIHONGyu19980918@cluster0.wg0g5.mongodb.net/<dbname>?retryWrites=true&w=majority";
+//|| 'mongodb://localhost:27017/questiontester'
 
-// // Connection to DB
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/questiontester', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//       console.log('Backend Started');
-//     })
-//     .catch(err => {
-//         console.error('Backend error:', err.stack);
-//         process.exit(1);
-//     });
-
-
-//connect Database.
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-  console.log("Mongodb database connection is on");
-});
+// Connection to DB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('Backend Started');
+    })
+    .catch(err => {
+        console.error('Backend error:', err.stack);
+        process.exit(1);
+    });
 
 const questionsRouter = require('./routes/questions');
 const usersRouter = require('./routes/users');
